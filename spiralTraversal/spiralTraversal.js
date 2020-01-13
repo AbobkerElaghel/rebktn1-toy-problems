e55798b39210a798506fe038fc8e0387ad075ed6
BigBBaker <abobkerelaghel@gmail.com>
2020-01-13 10:19:35 +0100

    almost solved

---
+++ spiralTraversal/spiralTraversal.js
@@ -0,0 +1,82 @@
+/*
+Spiral Traversal
+
+Write a function that accepts a 2-dimensional array (that is, an array containing many same-length arrays),
+and prints out every value found, but in a spiral from the upper left in to the center.
+
+spiralTraversal([ 
+  [ 1, 2, 3, 4, 5 ], 
+  [ 6, 7, 8, 9, 10 ], 
+  [ 11, 12, 13, 14, 15 ], 
+  [ 16, 17, 18, 19, 20 ], 
+  [ 21, 22, 23, 24, 25 ] 
+]) 
+
+// output: [ 1, 2, 3, 4, 5, 10, 15, 20, 25, 24, 23, 22, 21, 16, 11, 6, 7, 8, 9, 14, 19, 18, 17, 12, 13 ]
+
+
+spiralTraversal([ 
+  [ 1, 2, 3, 4, 5 ], 
+  [ 6, 7, 8, 9, 10 ], 
+  [ 11, 12, 13, 14, 15 ], 
+  [ 16, 17, 18, 19, 20 ], 
+  [ 21, 22, 23, 24, 25 ] 
+])	
+
+// output: [ 1, 2, 3, 4, 5, 10, 15, 20, 25, 24, 23, 22, 21, 16, 11, 6, 7, 8, 9, 14, 19, 18, 17, 12, 13 ]
+
+spiralTraversal([ 
+  [ 1, 2, 3 ], 
+  [ 4, 5, 6 ], 
+  [ 7, 8, 9 ], 
+  [ 10, 11, 12 ], 
+  [ 13, 14, 15 ], 
+  [ 16, 17, 18 ], 
+  [ 19, 20, 21 ], 
+  [ 22, 23, 24 ] 
+])	
+
+// output: [ 1, 2, 3, 6, 9, 12, 15, 18, 21, 24, 23, 22, 19, 16, 13, 10, 7, 4, 5, 8, 11, 14, 17, 20 ]
+
+spiralTraversal([[ 1 ], [ 2 ], [ 3 ], [ 4 ]])	// [ 1, 2, 3, 4 ]
+
+spiralTraversal([[ 1, 2, 3, 4, 5, 6, 7 ]]) // [ 1, 2, 3, 4, 5, 6, 7 ]
+*/
+spiralTraversal = matrix => {
+    let begingRow = 0;
+    let begingColuon = (matrix[0].length - 1);
+
+    for (let index = 0; index < matrix.length; index++) {
+
+        for (let i = begingRow; i < begingColuon; i++) {
+            console.log(matrix[begingRow][i])
+        }
+
+        for (let i = begingRow; i < begingColuon; i++) {
+            console.log(matrix[i][begingColuon])
+        }
+
+        for (let i = begingColuon; i > begingRow; i--) {
+            console.log(matrix[begingColuon][i]);
+        }
+
+        for (let i = begingColuon; i > begingRow; i--) {
+            console.log(matrix[i][begingRow]);
+        }
+        begingRow++;
+        begingColuon--;
+
+    }
+}
+
+
+spiralTraversal([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [10, 11, 12],
+    [13, 14, 15],
+    [16, 17, 18],
+    [19, 20, 21],
+    [22, 23, 24]
+]);
