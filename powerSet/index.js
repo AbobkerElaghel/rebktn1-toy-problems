23b96f257e2f1915c0c262a3e4fea9c226dc7a77
Abobker Elaghel <57757187+AbobkerElaghel@users.noreply.github.com>
2020-01-28 10:06:24 +0100

    almost thier

--- powerSet/index.js
+++ powerSet/index.js
@@ -14,10 +14,27 @@ Example:
 
 powerSet("a") // [ "", "a" ]
 powerSet("ab") // [ "", "a", "ab", "b" ]
+a b m o
+0 1 2 3
+0 01 012 0123 013 02 023 03 1 12 123 13 2 23 3
 powerSet("obama") // [ "", "a", "ab", "abm", "abmo", "abo", "am", "amo", "ao", "b", "bm", "bmo", "bo", "m", "mo", "o" ]
-powerSet("horse") // [ "", "e", "eh", "eho", "ehor", "ehors", "ehos", "ehr", "ehrs", "ehs", "eo", "eor", "eors", "eos", "er", "ers", "es", "h", "ho", "hor", "hors", "hos", "hr", "hrs", "hs", "o", "or", "ors", "os", "r", "rs", "s" ]
+e h o r s
+0 1 2 3 4
+
+0 1 2 3 4 01 02 03 04 12 13 23 24 34 012 013 014 0123 0124 0134 023 0234 024  034  123 1234 124  134   234  01234
+powerSet("horse") // [ "", "e", "eh", "eho", "ehor", "ehors", "ehos" , "ehr", "ehrs", "ehs",   "eo", "eor", "eors",
+"eos", "er", "ers", "es", "h", "ho", "hor", "hors", "hos", "hr", "hrs", "hs", "o", "or", "ors", "os", "r", "rs", "s" ]
 */
 
-function powerSet(string) {
-  // your code here...
+function _removeDuplicates(array) {
+    return array.filter( (a, b) => array.indexOf(a) === b)
 }
+
+function powerSet(string) {
+    let visited = [];
+    let arr = string.split("").sort();
+    arr = _removeDuplicates(arr);
+    console.log(arr)
+}
+
+powerSet("horse");
