2e3bf4e14a600ca85d9c8286c5450f247c8ffb28
Abobker Elaghel <57757187+AbobkerElaghel@users.noreply.github.com>
2020-02-03 09:18:18 +0100

    solved

--- sumOfPrimes/sumOfPrime.js
+++ sumOfPrimes/sumOfPrime.js
@@ -2,7 +2,30 @@
 Sum of Primes
 Given a positive number, return the sum of all positive primes that are less than or equal to that number.
 */
+primeTester = n => {
+    if (n === 1) {
+        return false;
+    }
+    //Leaner Time Complexity//
+
+    for (let i = Math.floor(Math.sqrt(n)); i > 1; i--) {
+        if ((n / i) % 1 === 0) {
+            return false;
+        }
+    }
+    return true;
+};
 
 function sumOfPrimes(n) {
-    // your code here...
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+        if(primeTester(i)){
+            sum+=i;
+        }
+    }
+    return sum;
 }
+
+console.log(sumOfPrimes(13));
+
+
