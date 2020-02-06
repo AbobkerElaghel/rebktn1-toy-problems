5432da781061f78b2d708ef3371cb9022e681d09
Abobker Elaghel <57757187+AbobkerElaghel@users.noreply.github.com>
2020-02-06 08:56:24 +0100

    *

--- coinSums/coinSums.js
+++ coinSums/coinSums.js
@@ -1,5 +1,5 @@
 /*
-In England the currency is made up of pound, £, and pence, p, and there are eight coins in general circulation:
+In England, the currency is made up of pound, £, and pence, p, and there are eight coins in general circulation:
 1p
 2p
 5p
@@ -12,15 +12,16 @@ In England the currency is made up of pound, £, and pence, p, and there are eig
 Given a given number of pence, return the possible number of ways someone could make change.
 It is possible to make 5 pence in the following ways:
 5 * 1p
-3 * 1p + 1 * 2p
+3 * 1p + 1 * 2p ???
 1 * 1p + 2 * 2p
 1 * 5p
 In other words, find all the possible combinations of coins that sum to a given pence value.
 */
 
 // HELPERS
-var coins = [1, 2, 5, 10, 20, 50, 100, 200];
+const coins = [1, 2, 5, 10, 20, 50, 100, 200];
 
-function coinSums(total) {
+const coinSums = total => {
   // your code here...
-}
+
+};
