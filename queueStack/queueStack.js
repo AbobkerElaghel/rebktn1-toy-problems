47dfe1dc49e9735b38c6432307d6039093f4390d
Abobker Elaghel <57757187+AbobkerElaghel@users.noreply.github.com>
2020-02-17 09:59:40 +0100

    solved queueStack

--- queueStack/queueStack.js
+++ queueStack/queueStack.js
@@ -5,23 +5,51 @@ Write a stack. Once you’re done, implement a queue using two stacks to create 
 The queue should not have any storage separate from its stacks.
 */
 
-var Stack = function() {
-  var storage = [];
+const Stack = function() {
+  let storage = [];
 
-  this.push = function() {};
+  this.push = function(value) {
+    storage.push(value);
+    return value;
+  };
 
-  this.pop = function() {};
+  this.pop = function() {
+    return storage.pop();
+  };
 
-  this.size = function() {};
+  this.size = function() {
+    return storage.length;
+  };
 };
 
-var Queue = function() {
-  var inbox = new Stack();
-  var outbox = new Stack();
+const Queue = function() {
+  let inbox = new Stack();
+  let outbox = new Stack();
 
-  this.enqueue = function() {};
-
-  this.dequeue = function() {};
-
-  this.size = function() {};
+  this.enqueue = function(value) {
+    inbox.push(value);
+  };
+  this.dequeue = function() {
+    debugger
+    if(outbox.size()){
+      return outbox.pop();
+    }else if (inbox.size()){
+      for (let i = 0; i < inbox.size() - 1 ; i++) {
+        outbox.push(inbox.pop());
+      }
+      return outbox.pop();
+    }else{
+      throw new Error("The Queue is Empty");
+    }
+  };
+  this.size = function() {
+    return inbox.size() + outbox.size();
+  };
 };
+
+
+let stack = new Stack();
+
+
+stack.push(1);stack.push(1);stack.push(1);
+console.log(stack.size());
