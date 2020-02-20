1aa2659e4564866317ef2087fef5c607a812bb54
Abobker Elaghel <57757187+AbobkerElaghel@users.noreply.github.com>
2020-02-20 09:35:56 +0100

    solved RomanNumeralTranslator

--- RomanNumeralTranslator/RomanNumeralTranslator.js
+++ RomanNumeralTranslator/RomanNumeralTranslator.js
@@ -11,20 +11,43 @@
 // CM = 900 (1000 – 100 = 900)
 
 // You should return “null” on invalid input.
-
 // You've got Helpers!
-
-// var DIGIT_VALUES = {
-//     I: 1,
-//     V: 5,
-//     X: 10,
-//     L: 50, 
-//     C: 100,
-//     D: 500,
-//     M: 1000
-//   };
-
-function translateRomanNumeral (romanNumeral) {
-    // Write your code here, and
-    // return your final answer.
+const translateRomanNumeral = romanNumeral => {
+  if(typeof romanNumeral !== "string"){
+    throw new TypeError("input must be a string");
   }
+
+  let re = /^(?=[MDCLXVI])M*(C[MD]|D?C{0,3})(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})$/;
+  if(!re.test(romanNumeral)){
+    throw new Error("input is invalid");
+  }
+
+  const DIGIT_VALUES = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000
+  };
+  if(romanNumeral.length === 1){
+    return DIGIT_VALUES[romanNumeral];
+  }
+  let totalSum = 0;
+  for (let i = 1; i < romanNumeral.length; i++) {
+    // M = 1000    C = 100    C = 100
+    if(DIGIT_VALUES[romanNumeral[i]] > DIGIT_VALUES[romanNumeral[i - 1]]){
+      totalSum = totalSum + (DIGIT_VALUES[romanNumeral[i]] - DIGIT_VALUES[romanNumeral[i-1]]);
+    }else{
+      if(i === 1){
+        totalSum+=(DIGIT_VALUES[romanNumeral[i]] + DIGIT_VALUES[romanNumeral[i-1]]);
+      }else{
+        totalSum+=DIGIT_VALUES[romanNumeral[i]];
+      }
+    }
+  }
+  return totalSum;
+  };
+
+console.log(translateRomanNumeral("XC"));
