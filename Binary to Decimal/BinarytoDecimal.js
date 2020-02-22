284181ea8df93ecebbee9a8bf33f7121599cd3b5
Abobker Elaghel <57757187+AbobkerElaghel@users.noreply.github.com>
2020-02-22 09:07:39 +0100

    solved BinaryToDecimal

--- Binary to Decimal/BinarytoDecimal.js
+++ Binary to Decimal/BinarytoDecimal.js
@@ -14,7 +14,28 @@
 // n:"1000"	 8 
 
 
-
+/**
+ *
+ * @param {string} n the binary string input
+ * @return {Number|TypeError} the result in decimal
+ */
 function binaryToDecimal (n) {
-// code gose here 
+    let base = 0;
+    let result = 0;
+    if(typeof n !== 'string'){
+        return new TypeError("input must be a binary string");
+    }
+    if(!(/^[0-1]+$/.test(n))){
+        return new TypeError("input is invalid, must be binary string");
+    }
+
+    for (let i = n.length - 1; i >= 0 ; i--) {
+        if(n[i] == 1){
+            result+=Math.pow(2,base);
+        }
+        base++;
+    }
+    return result;
 }
+
+console.log(binaryToDecimal('1111110'));
